41654d51aaf603c3e457b8627db397668dfbb919
asus <wjct333@gmail.com>
2018-09-29 14:16:55 +1000

    Creating year_month for x-axis; Not able to combine year with month, as data is showing as strings rather than integer

--- Weather.py
+++ Weather.py
@@ -1,5 +1,31 @@
 # Importing weather data
 
+import matplotlib.pyplot as plt
+import csv
+
 file = open('Data/Rainfall Data.csv',"r")
-print(file.read())
-file.close()
+
+reader = csv.reader(file)         # Alls to create list
+
+print(reader)
+
+for row in reader:
+
+    years = row[2]
+    months = row[3]
+    rainfall = row[4]
+    # print(list(int(years)))
+    year_month = []
+    for year in years:
+        for month in months:
+            year_month = years.append(months)
+            labelled = '-'.join([str(year),month])
+            year_month.append(labelled)
+    print(year_month)
+
+# plt.plot(year_month, rainfall)
+# plt.ylabel('month')
+# plt.show()
+
+# file.close()
+
