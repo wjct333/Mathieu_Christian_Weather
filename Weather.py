e7ca79560846d8de282735fe753c29f6d92df6cf
asus <wjct333@gmail.com>
2018-10-01 15:30:41 +1000

    1.Updated the code to skip empty string.

--- Weather.py
+++ Weather.py
@@ -10,20 +10,24 @@ reader = csv.reader(file)
 
 result = []                # Extracting and combining year and month data
 rainfall = []
+next(reader)
 for row in reader:
+    if row[4] == '':
+        continue
     rainfall.append((row[4]))
     result.append(row[2] + '-' + row[3])
 rainfall = rainfall[1:-1]
 result = result[1:-1]
 print(rainfall)
 print(result)
-
-# Plot linear relationship
-# fig = plt.figure(figsize=(20,20))
-# plt.plot(result,rainfall)  # Plotting graph on rainfall vs year_month
-# plt.xlabel('Year_Month')
-# plt.ylabel('rainfall')
-
+#
+# ''' Plot linear relationship '''
+fig = plt.figure(figsize=(20,20))
+plt.plot(result,rainfall)  # Plotting graph on rainfall vs year_month
+plt.xlabel('Year_Month')
+plt.ylabel('rainfall')
+#
+# ''' Plot barchart '''
 y_pos = np.arange(len(result))
 plt.bar(y_pos, rainfall, align='center', alpha=0.5)
 plt.xticks(y_pos, result)
--- scratch_weather.py
+++ scratch_weather.py
@@ -1,27 +1,27 @@
-import matplotlib.pyplot as plt
-
-years = [2014,2015,2016,2017,2018]
-
-months = ["Jan", "Feb"]
-
-rainfalls = [11, 22, 31, 12, 43, 26, 42, 12, 24, 23]
-
-def plot_weather_precipitation():
-    pass
-year_month = []                # Extracting and combining year and month data
-precipitation = []
-for year in years:
-    for month in months:
-        year_month.append(str(year) + '-' + month)
-
-for rainfall in rainfalls:
-    precipitation.append(rainfall)
-print (year_month, precipitation)
-
-plt.plot(year_month, precipitation)
-plt.xlabel('Year_Month')
-plt.ylabel('Precipitation')
-plt.show()
+# import matplotlib.pyplot as plt
+#
+# years = [2014,2015,2016,2017,2018]
+#
+# months = ["Jan", "Feb"]
+#
+# rainfalls = [11, 22, 31, 12, 43, 26, 42, 12, 24, 23]
+#
+# def plot_weather_precipitation():
+#     pass
+# year_month = []                # Extracting and combining year and month data
+# precipitation = []
+# for year in years:
+#     for month in months:
+#         year_month.append(str(year) + '-' + month)
+#
+# for rainfall in rainfalls:
+#     precipitation.append(rainfall)
+# print (year_month, precipitation)
+#
+# plt.plot(year_month, precipitation)
+# plt.xlabel('Year_Month')
+# plt.ylabel('Precipitation')
+# plt.show()
 
 
 
@@ -36,3 +36,7 @@ plt.show()
 
 # Error message:
 # Matplotlib: ValueError: x and y must have same first dimension
+
+# a = ('1','2','3','4','5','6')
+
+print(float())
