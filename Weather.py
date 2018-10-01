712a0419110918d360b154b1f72d6af67ff9e526
asus <wjct333@gmail.com>
2018-10-01 15:57:11 +1000

    2.Including temperature vs time graph.

--- Weather.py
+++ Weather.py
@@ -5,34 +5,37 @@ import numpy as np
 def plot_weather_precipitation():
     pass
 
-file = open('Weather Data_CSV.csv',"r") # Importing weather data
+file = open('Weather Data_CSV.csv',"r") # Import data
 reader = csv.reader(file)
 
-result = []                # Extracting and combining year and month data
+result = []
 rainfall = []
+temperature = []
 next(reader)
 for row in reader:
     if row[4] == '':
         continue
     rainfall.append(float(row[4]))
+    temperature.append(float(row[5]))
     result.append(row[2] + '-' + row[3])
-rainfall = rainfall[1:-1]
-result = result[1:-1]
 print(rainfall)
+print(temperature)
 print(result)
-#
-# ''' Plot linear relationship '''
-fig = plt.figure(figsize=(20,20))
-plt.plot(result,rainfall)  # Plotting graph on rainfall vs year_month
-plt.xlabel('Year_Month')
-plt.ylabel('rainfall')
-#
+
 # ''' Plot barchart '''
+fig = plt.figure(figsize=(20,20))
 y_pos = np.arange(len(result))
 plt.bar(y_pos, rainfall, align='center', alpha=0.5)
 plt.xticks(y_pos, result)
 plt.ylabel('rainfall')
 plt.title('Weather Data')
+
+# ''' Plot linear relationship '''
+fig = plt.figure(figsize=(20,20))
+plt.plot(result,temperature)  # Plotting graph on rainfall vs year_month
+plt.xlabel('Year_Month')
+plt.ylabel('Temperature')
+
 
 plt.show()
 
