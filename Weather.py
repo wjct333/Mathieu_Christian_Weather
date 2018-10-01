9844e83d37867eb042cdd3abed683fcc0a8000c3
asus <wjct333@gmail.com>
2018-10-01 11:57:03 +1000

    1. Remove 1st and last element of the rainfall and time list. 2. Draw bar chart graph for relationship between time and rainfall.

--- Weather.py
+++ Weather.py
@@ -13,12 +13,22 @@ rainfall = []
 for row in reader:
     rainfall.append((row[4]))
     result.append(row[2] + '-' + row[3])
-print(result, rainfall)
+rainfall = rainfall[1:-1]
+result = result[1:-1]
+print(rainfall)
+print(result)
 
-fig = plt.figure(figsize=(20,20))
-plt.plot(result,rainfall)  # Plotting graph on rainfall vs year_month
-plt.xlabel('Year_Month')
+# Plot linear relationship
+# fig = plt.figure(figsize=(20,20))
+# plt.plot(result,rainfall)  # Plotting graph on rainfall vs year_month
+# plt.xlabel('Year_Month')
+# plt.ylabel('rainfall')
+
+y_pos = np.arange(len(result))
+plt.bar(y_pos, rainfall, align='center', alpha=0.5)
+plt.xticks(y_pos, result)
 plt.ylabel('rainfall')
+plt.title('Weather Data')
 
 plt.show()
 
